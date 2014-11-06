911e724b23bd44dd17878d7218d96f9dd078f345
Jason Scott <jrscott@fullsail.edu>
2014-11-06 09:52:07 -0600

    made minor changes from previous screen casts
    made minor changes from previous screen casts

--- AssociatingJS/js/Script.js
+++ AssociatingJS/js/Script.js
@@ -1,19 +1,4 @@
-alert("This is my alert! Don't worry, it's not that serious... LOL")
 
-var yearBorn = 1983;
-
-var age = 31;
-
-var firstName = "Jason";
-
-var muppetNames = ["Grover" , "Fozzy" ,"Animal"]
-
-
-
-console.log(yearBorn)
-console.log (age)
-console.log (yearBorn)
-console.log(muppetNames[1]);
 
 alert("Welcome!")
 
@@ -21,3 +6,11 @@ var name = prompt ("Please type your name");
 
 alert ("Welcome "+name+"! Let's see how old you are.")
 var yearBorn = prompt ("What year were you born?");
+
+console.log (yearBorn);
+
+var currentYear = 2014;
+
+var age = currentYear - yearBorn;
+
+alert(name+" you are "+age+" years old.");
