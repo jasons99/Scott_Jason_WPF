89e664b31d1c8a94794b05c01d162ef401c27911
Jason Scott <jrscott@fullsail.edu>
2014-11-06 09:42:00 -0600

    Asked user for name and year born
    Asked user for name and year born

--- AssociatingJS/js/Script.js
+++ AssociatingJS/js/Script.js
@@ -2,4 +2,22 @@ alert("This is my alert! Don't worry, it's not that serious... LOL")
 
 var yearBorn = 1983;
 
-console.log(yearBorn);
+var age = 31;
+
+var firstName = "Jason";
+
+var muppetNames = ["Grover" , "Fozzy" ,"Animal"]
+
+
+
+console.log(yearBorn)
+console.log (age)
+console.log (yearBorn)
+console.log(muppetNames[1]);
+
+alert("Welcome!")
+
+var name = prompt ("Please type your name");
+
+alert ("Welcome "+name+"! Let's see how old you are.")
+var yearBorn = prompt ("What year were you born?");
