22f65f89ccbab884428b55743e0084868c89f9da
Jason Scott <jrscott@fullsail.edu>
2014-11-07 14:37:08 -0600

    expression worksheet
    expression worksheet

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,38 @@
+alert("Expression Worksheet")
+//Jason Scott Expression Worksheet WPF
+
+//Dog Years Problem
+
+alert ("Calculate your dogs age.")
+var dogsAge = prompt ("How old is the dog in human years?");
+
+console.log (dogsAge);
+
+var ageFactor = 7;
+
+var age = dogsAge * ageFactor;
+
+alert(" Your dog is "+age+" years old in dog years.");
+console.log (age);
+
+//Pizza party expression
+
+alert ("Lets have a pizza party.")
+var partyPeople = prompt ("How many people are coming to the party?");
+
+console.log (partyPeople);
+
+var totalPizzas = prompt ("How many pizzas are you going to order?")
+
+console.log (totalPizzas)
+
+var slicesPerPie = 10
+
+console.log (slicesPerPie)
+
+var slicesPerPerson = slicesPerPie * totalPizzas / partyPeople
+
+alert(" Each person will get "+slicesPerPerson+" slices of pizza.");
+
+console.log (slicesPerPerson)
+
