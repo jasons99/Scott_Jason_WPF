2e87a8bb5263771c6ecd498c45123be7ad185db6
Jason Scott <jrscott@fullsail.edu>
2014-11-07 22:28:08 -0600

    expressions assignment 2
    minor changes

--- Personal/Personal Example/js/script.js
+++ Personal/Personal Example/js/script.js
@@ -5,5 +5,10 @@ alert("Expression Worksheet")
 //Jason Scott Expression Assignment WPF
 
 //Personal example
+//How many tanks of gas do I use per year?
 
+alert ("Let's figure out how many tanks of of gas you will need to buy per year.")
+var tanksPerMonth = prompt ("How many times per month do you fill up with gas?");
+
+console.log (tanksPerMonth);
 
