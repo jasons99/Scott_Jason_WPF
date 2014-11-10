9525c130ed80be8bf009c0bddd57bffc2c482426
Jason Scott <jrscott@fullsail.edu>
2014-11-09 23:50:01 -0600

    Wacky example
    wacky example

---
+++ Wacky/Wacky example /js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by jasonscott on 11/9/14.
+ */
+/**
+ * Created by jasonscott on 11/9/14.
+ */
+alert("Expression Assignment")
+//Jason Scott Expression Assignment WPF
+
+//Wacky example
+//Calculate how much you made in tips this month from playing live gigs.
+
