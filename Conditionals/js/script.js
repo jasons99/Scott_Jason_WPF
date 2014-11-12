a9d601c6b1f51d2e44c6b6ac554685f405b9e1bd
Jason Scott <jrscott@fullsail.edu>
2014-11-11 20:43:56 -0600

    conditionals
    relations

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,9 +1,11 @@
 //Conditional Logic
 
-var oldEnough = true;
+var kidHeight = 47;
+var minHeight = 48
+var sneakerLift = 2
 
 //If the child is old enough, print to the console "you can ride!"
-if(oldEnough){
+if(kidHeight + sneakerLift > minHeight){
 
     console.log("You can ride!");
 }
