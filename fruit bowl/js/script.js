6153f6b26d0e5d45061a4b9ed042a9e096a85d66
Jason Scott <jrscott@fullsail.edu>
2014-11-12 19:48:17 -0600

    fruit bowl
    following along

--- fruit bowl/js/script.js
+++ fruit bowl/js/script.js
@@ -6,4 +6,53 @@ fruitBowl[0] = "pear";
 
 console.log(fruitBowl);
 
-console.log(fruitBowl.length); 
+console.log(fruitBowl.length);
+
+
+//check item one
+var numPears = 0;
+
+if (fruitBowl[0] === "pear"){
+
+    numPears ++;
+}else{
+
+    console.log("This fruit is not a pear");
+}
+//check item two
+if (fruitBowl[1] === "pear"){
+
+    numPears ++;
+}else{
+
+    console.log("This fruit is not a pear");
+}
+
+
+console.log(numPears);
+//check item three
+if (fruitBowl[2] === "pear"){
+
+    numPears ++;
+}else{
+
+    console.log("This fruit is not a pear");
+}
+//check item four
+if (fruitBowl[3] === "pear"){
+
+    numPears ++;
+}else{
+
+    console.log("This fruit is not a pear");
+}
+
+var name = prompt("What is your name?");
+console.log(name);
+
+if (name === ""){
+
+    name = prompt("Please enter your name.")
+}
+
+console.log(name);
