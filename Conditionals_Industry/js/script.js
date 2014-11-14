21a9d79ff5daa1739753afc5702cf3abe2c22adb
Jason Scott <jrscott@fullsail.edu>
2014-11-13 20:50:26 -0600

    conditionals industry
    still working

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -11,3 +11,7 @@ alert("Let's see if you need to upgrade your end users anti virus software.");
 
 //Let's assume that all users are using the same version of software
 
+var lastCheck = prompt("Have you checked for an update in the last six months?");
+
+if (lastCheck === ""){
+    lastCheck = prompt("Please type yes or no if you checked the software versions.");
