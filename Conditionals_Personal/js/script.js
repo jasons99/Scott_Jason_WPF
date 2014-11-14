394186a01427710b433d06bafc5eca3073f4454d
Jason Scott <jrscott@fullsail.edu>
2014-11-13 19:36:07 -0600

    conditionals personal
    trying different ideas

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -12,4 +12,27 @@ alert("Let's see if you can go to the Bruce Springsteen concert next weekend!");
 
 var work = prompt("Do you have to work next weekend?");
 
+if (work = ""){
+    prompt("Do you have to work?");
+}
+if (work === "no"){
+    alert("Great! Looks like you're free to go to the show! But wait. First we have to see if you can afford to go.");
+    console.log(work)
+    var myMoney = prompt("How much cash do you have?");
+}
+else
+    alert("Sorry pal. You gotta go to work. :(");
 
+console.log("Concert is a no go.");
+
+var ticketPrice = 200.00
+
+if (myMoney >=ticketPrice){
+    alert("Great! You've got enough to buy those tickets!")
+    console.log(myMoney);
+    alert("Enjoy the show!!!");
+    console.log("Enjoy the show!");
+}
+else
+    alert("Sorry guy. Looks like you can't afford to go.");
+console.log("You don't have enough money.");
