6497f8078cfbd8f3fd2947d31794fd04d2d5886d
Jason Scott <jrscott@fullsail.edu>
2014-11-13 19:29:48 -0600

    conditionals personal
    starting example 1

---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jasonscott on 11/13/14.
+ */
+/**
+ * Created by jasonscott on 11/13/14.
+ */
+//Conditionals Personal
+//Jason Scott Nov 13th 2014
+//Can you go to the Bruce Springsteen concert next weekend?
+
