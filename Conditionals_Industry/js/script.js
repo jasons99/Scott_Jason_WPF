d74694a65f11e89bea157ee5972eba6642573d5f
Jason Scott <jrscott@fullsail.edu>
2014-11-13 20:49:51 -0600

    conditionals industry
    new idea

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,3 +4,10 @@
 
 //Conditionals Industry
 //Jason Scott Nov 13th 2014
+
+//Let's see if you need to upgrade your end users anti virus software
+
+alert("Let's see if you need to upgrade your end users anti virus software.");
+
+//Let's assume that all users are using the same version of software
+
