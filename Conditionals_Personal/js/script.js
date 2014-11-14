a070fb17da871378816d296ffc8559dba5dbf8b4
Jason Scott <jrscott@fullsail.edu>
2014-11-13 19:30:12 -0600

    conditionals personal
    2

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -8,3 +8,8 @@
 //Jason Scott Nov 13th 2014
 //Can you go to the Bruce Springsteen concert next weekend?
 
+alert("Let's see if you can go to the Bruce Springsteen concert next weekend!");
+
+var work = prompt("Do you have to work next weekend?");
+
+
