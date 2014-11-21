f9ff2fcea8dc96162103dace590f8735a0b11f64
Jason Scott <jrscott@fullsail.edu>
2014-11-20 21:35:11 -0600

    functions personal
    2

--- Functions_Personal/script/script.js
+++ Functions_Personal/script/script.js
@@ -5,5 +5,6 @@
 //Jason Scott personal functions example 11/20/2014
     //Picking Lottery numbers
 
+alert("Let's pick some lottery numbers for this week.")
+alert("We will determine a range of numbers to choose from and the computer will pick them out for us.")
 
-}
