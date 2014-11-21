4b72d77bfe78589f791b112da051232889a22fed
Jason Scott <jrscott@fullsail.edu>
2014-11-20 22:30:32 -0600

    functions industry
    5

--- Functions_Industry/script/script.js
+++ Functions_Industry/script/script.js
@@ -24,3 +24,18 @@ var total = calcArea(h,w);
 function calcArea(w, h){
     var area = w * h
     return area;
+
+}
+alert("The image has a pixel area of " +total);
+
+alert("The header area on your web page is 120 X 600 pixels.");
+
+
+if (total <=72000){
+    alert("Ok good. Looks like your image will fit in the header place!.");
+    console.log("Success");
+
+}
+else
+    alert("Ok. It looks like that image won't fit. You better resize it bud.");
+console.log("Too big. ");
