7fa1ef86983c994efbc5a69891ddc075054bbf10
Jason Scott <jrscott@fullsail.edu>
2014-11-20 22:29:13 -0600

    functions industry
    2

--- Functions_Industry/script/script.js
+++ Functions_Industry/script/script.js
@@ -1,3 +1,13 @@
 //Jason Scott industry functions example 11/20/2014
 
 alert("We need to see if that new graphic logo you designed will fit in the header box for the web page.")
+
+
+var h = prompt("Type the height in pixels");
+
+while(isNaN(h) || h===""){
+
+    h = prompt("Please enter a number!");
+}console.log(h);
+
+
