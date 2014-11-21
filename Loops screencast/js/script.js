a4faad81ec794dd9d5237f53754042b3c4799b00
Jason Scott <jrscott@fullsail.edu>
2014-11-20 19:22:41 -0600

    loops
    screen cast

--- Loops screencast/js/script.js
+++ Loops screencast/js/script.js
@@ -2,10 +2,28 @@
  * Created by jasonscott on 11/20/14.
  */
 
-
+//while loop
+    
 var b = 10;
 
 while (b > 0){
     console.log(b + "test");
     b--;
+}
+
+//do while loops
+
+var c = 10
+
+do{
+    console.log(c + "test")
+    c--;
+}
+while(c > 0);
+
+
+//for loop
+
+for (var d = 10; d > 0; d--){
+    console.log(d + "test")
 }
